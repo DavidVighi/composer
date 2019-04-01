6a7c66b26a83231215281b18d5e8e96f82de53ce
David Vighi <david.vighi@gmail.com>
2019-04-01 12:35:06 +0200

    Composer debut corrige

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@ CONST BR = '<br/>';
 
 require __DIR__.'/../vendor/autoload.php';
 
-require "../src/wcs/Hello.php";
-
 $hello = new Hello();
 $helloworld = new namespace\HelloWorld\SayHello();
 echo $hello->Talk();
